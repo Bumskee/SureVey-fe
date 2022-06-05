663f002ef00e070bb22d31fe32aed2663c1e52c8
Bumskee <fadhlan.razan@binus.ac.id>
2022-06-05 20:05:38 +0700

    user can now authenticate themself using their credentials, register is now functional, changed the css of the App a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Mainbody from "./components/Home/Mainbody";
 import Template from "./components/Home/Template";
 import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
+import Test from "./components/Tests/Test"
 import "./App.css";
 import CenterTabs from "./components/Form/Tabs";
 import Questionform from "./components/Form/Questionform";
 
 const App = () => {
+
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            <Link className="nav_text" to="/">Home</Link>
+            <Link className="nav_text" to="/login">Login</Link>
+            {!isAuth ? <Link className="nav_text" to="/login">Auth_Test</Link> : <Link className="nav_text" to="/test">Auth_Test</Link>}
         </nav>
         <Routes>
             <Route path="/" element={
@@ -29,8 +34,9 @@ const App = () => {
             </div>
             }>
             </Route>
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/login" element={< Login setIsAuth={setIsAuth} />}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/test" element={<Test/>}/>
         </Routes>
     </Router>
   );
